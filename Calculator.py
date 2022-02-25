a679b5a93311ccd417f620aa718545f7a46afaea
Deepika2416 <deepika.gupta.clt@gmail.com>
2022-02-24 21:30:56 -0500

    Sub method added

--- Calculator.py
+++ Calculator.py
@@ -2,9 +2,13 @@ class Calculator():
     def add(self,a,b):
         c=a+b
         return c
+    def sub(self,a,b):
+        return a-b
 
 
 
 object=Calculator()
 d=object.add(2,3)
 print(d)
+e=object.sub(6,5)
+print(e)
